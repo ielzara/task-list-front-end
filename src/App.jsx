8b453f15b10f8b5dba640e9b07b165365af45b5d
Elzara Turakulova <endjik@gmail.com>
2024-12-18 10:36:18 -0800

    wave 3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -10,18 +11,39 @@ const TASKS = [
   {
     id: 2,
     title: 'Cook Pasta',
-    isComplete: true,
+    isComplete: false,
   },
 ];
 
 const App = () => {
+  const [tasks, setComplete] = useState(TASKS);
+
+  const updateTaskStatus = (id) => {
+    setComplete(tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isComplete : !task.isComplete};
+      } else {
+        return task;
+      }
+    }));
+  };
+
+  const deleteTask = id => {
+    setComplete(tasks =>tasks.filter(task => task.id !== id));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          {<TaskList tasks={tasks}
+            onTaskToggle={updateTaskStatus}
+            onDeleteTask={deleteTask}
+          />}
+        </div>
       </main>
     </div>
   );
